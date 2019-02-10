38d8bc8d53b92195f0a81eb9cb6197d780b1a948
eduardojoya <ebjoya@gmail.com>
2019-02-09 20:27:49 -0800

    combined top and bottom html with blog content and created bash script for blog

--- docs/build.sh
+++ docs/build.sh
@@ -2,3 +2,4 @@ cat templates/top.html content/index.html templates/bottom.html > docs/index.htm
 cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
 cat templates/top.html content/resume.html templates/bottom.html > docs/education.html
 cat templates/top.html content/interests.html templates/bottom.html > docs/interests.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
