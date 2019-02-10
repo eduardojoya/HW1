81813c11c748a38e40605048e1552d85e2fa564a
eduardojoya <ebjoya@gmail.com>
2019-02-09 20:22:40 -0800

    combined top, bottom html with interests content and added bash script for interests

--- docs/build.sh
+++ docs/build.sh
@@ -1,3 +1,4 @@
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
 cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
-cat templates/top.html contnt/resume.html templates/bottom.html > docs/education.html
+cat templates/top.html content/resume.html templates/bottom.html > docs/education.html
+cat templates/top.html content/interests.html templates/bottom.html > docs/interests.html
