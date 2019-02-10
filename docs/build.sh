af095fe5a63bd064a43c81c50d4c88c8f22553eb
eduardojoya <ebjoya@gmail.com>
2019-02-09 20:18:00 -0800

    combined top bottom and education and updated bashscript to include education.html

--- docs/build.sh
+++ docs/build.sh
@@ -1,3 +1,3 @@
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
 cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
-
+cat templates/top.html contnt/resume.html templates/bottom.html > docs/education.html
